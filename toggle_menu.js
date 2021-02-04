a1b3ebda0c9fd07fdfc2c06da1b137d6cc31069b
Md Mehadi Hasan <hmd.mehadi@outlook.com>
2021-02-04 15:06:04 +0100

    first commit

---
+++ toggle_menu.js
@@ -0,0 +1,13 @@
+
+            var MenuItem=document.getElementById("MenuItems");
+            MenuItem.style.maxHeight="0px";
+            function menuetoggle(){
+                if(MenuItem.style.maxHeight=="0px"){
+                    MenuItem.style.maxHeight="250px";
+                }
+                else{
+                    MenuItem.style.maxHeight="0px";
+                }
+    
+            }
+       
